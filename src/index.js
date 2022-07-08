abbb8101b16b5577d8616d7874f884f2e1a1cbea
LeonardRodri <rodriguespeixoto.1995@gmail.com>
2022-07-07 23:24:56 -0300

    First Commint

--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find((user) => user.username === username);
+  
+  if(!user) {
+    return response.status(400).json({ erro: `O usuario ${user} não possuiu cadastro`});
+  }
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.query;
+  const id = uuidv4();
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos:[]
+  })
+  return response.json(users);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+
+  return response.json(user.todos);
+
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
